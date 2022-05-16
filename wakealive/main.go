e516fe044ac876237cd6752e0282036b43a6d5f0
Destin Moulton <destin.moulton@gmail.com>
2022-05-16 15:14:08 -0600

    Check the temp png for icon, restart i3 if not found

--- wakealive/main.go
+++ wakealive/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"utiligo/lib"
 )
 
@@ -18,12 +19,39 @@ func main() {
 	// 	fmt.Printf("%d %s %s\n", proc.Pid(), proc.Binary(), proc.Cmdline())
 	//	}
 	// }
-	proc, err := lib.Find("dropbox")
+	fmt.Printf("temp dir %s\n", os.TempDir())
+
+	// Get the system tray and save as png
+	img, err := lib.ImageTools.SystrayShot(120)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	filename, err := lib.ImageTools.SaveAsTempPNG(img)
+	if err != nil {
+		fmt.Printf("unable to save temp png: %v\n", err)
+		os.Exit(1)
+	}
+	small := "dropbox_icon.png"
+	isWithin := lib.ImageTools.IsImageWithin(small, filename)
+	os.Remove(filename)
+	fmt.Printf("Is %s within %s? %v\n", small, filename, isWithin)
+
+	// Check if the process is running
+	p := "dropbox"
+	isRunning, err := lib.Processes.IsRunning(p)
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%d %s", proc.Pid(), proc.Binary())
+	fmt.Printf("Is %s running? %v\n", p, isRunning)
+
+	if !isWithin && isRunning {
+		// Reload i3
+		fmt.Println("Restarting i3")
+		cmd := exec.Command("/usr/bin/i3-msg", "restart")
+		cmd.Run()
+	}
 }
