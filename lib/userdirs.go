854504943b110ddda0c895424e683a4f01f1b09a
Destin Moulton <destin.moulton@gmail.com>
2022-05-16 18:09:15 -0600

    Added local and logs userdirs and a method to make dirs

--- lib/userdirs.go
+++ lib/userdirs.go
@@ -1,34 +1,62 @@
 package lib
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
-type XDGDirs struct {
+type Dirs struct {
 	config string
 	cache  string
+	local  string
+	logs   string
 }
 
-var UserDirs XDGDirs
+var UserDirs = Dirs{}
 
-func init() {
+func (d *Dirs) Init(appname string) {
+	module := "go-utils"
 	if os.Getenv("XDG_CONFIG_HOME") != "" {
-		UserDirs.config = os.Getenv("XDG_CONFIG_HOME")
+		d.config = filepath.Join(os.Getenv("XDG_CONFIG_HOME"), module, appname)
 	} else {
-		UserDirs.config = filepath.Join(os.Getenv("HOME"), ".config")
+		d.config = filepath.Join(os.Getenv("HOME"), ".config", module, appname)
 	}
 	if os.Getenv("XDG_CACHE_HOME") != "" {
-		UserDirs.cache = os.Getenv("XDG_CACHE_HOME")
+		d.cache = filepath.Join(os.Getenv("XDG_CACHE_HOME"), module, appname)
 	} else {
-		UserDirs.cache = filepath.Join(os.Getenv("HOME"), ".cache")
+		d.cache = filepath.Join(os.Getenv("HOME"), ".cache", module, appname)
 	}
+
+	d.local = filepath.Join(os.Getenv("HOME"), ".local", module, appname)
+	d.logs = filepath.Join(d.local, "logs")
+
+	makeUserDir(d.config)
+	makeUserDir(d.cache)
+	makeUserDir(d.local)
+	makeUserDir(d.logs)
 }
 
-func (x *XDGDirs) Config() string {
+func (x *Dirs) Config() string {
 	return x.config
 }
 
-func (x *XDGDirs) Cache() string {
+func (x *Dirs) Cache() string {
 	return x.cache
+}
+
+func (x *Dirs) Local() string {
+	return x.local
+}
+
+func (x *Dirs) Logs() string {
+	return x.logs
+}
+
+func makeUserDir(path string) {
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		fmt.Printf("error creating userdir %s %v", path, err)
+		os.Exit(1)
+	}
 }
