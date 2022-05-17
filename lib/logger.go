226475ec1c760cb0f1416c5b16c64804944c8a6a
Destin Moulton <destin.moulton@gmail.com>
2022-05-16 18:43:37 -0600

    Created logger using logrus and lumberjack

---
+++ lib/logger.go
@@ -0,0 +1,33 @@
+package lib
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type LoggerData struct{}
+
+var Logger LoggerData
+
+func (l *LoggerData) SetupLogger(logpath string) {
+
+	lumberjackLogger := &lumberjack.Logger{
+		// Log file abbsolute path, os agnostic
+		Filename:   filepath.ToSlash(filepath.Join(logpath, "logs")),
+		MaxSize:    5, // MB
+		MaxBackups: 10,
+		MaxAge:     30,   // days
+		Compress:   true, // disabled by default
+	}
+
+	// Fork writing into two outputs
+	multiWriter := io.MultiWriter(os.Stderr, lumberjackLogger)
+
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+	log.SetOutput(multiWriter)
+}
