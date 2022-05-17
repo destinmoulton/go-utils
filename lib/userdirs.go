f9d2efbe83fa9673c09eb9185b0a5ea15bd7f7bb
Destin Moulton <destin.moulton@gmail.com>
2022-05-16 18:19:24 -0600

    Converted to map

--- lib/userdirs.go
+++ lib/userdirs.go
@@ -6,57 +6,54 @@ import (
 	"path/filepath"
 )
 
-type Dirs struct {
-	config string
-	cache  string
-	local  string
-	logs   string
+type DirData struct {
+	dirs map[string]string
 }
 
-var UserDirs = Dirs{}
+var UserDirs DirData
 
-func (d *Dirs) Init(appname string) {
+func (d *DirData) Init(appname string) {
 	module := "go-utils"
+	d.dirs = make(map[string]string)
 	if os.Getenv("XDG_CONFIG_HOME") != "" {
-		d.config = filepath.Join(os.Getenv("XDG_CONFIG_HOME"), module, appname)
+		d.dirs["config"] = filepath.Join(os.Getenv("XDG_CONFIG_HOME"), module, appname)
 	} else {
-		d.config = filepath.Join(os.Getenv("HOME"), ".config", module, appname)
+		d.dirs["config"] = filepath.Join(os.Getenv("HOME"), ".config", module, appname)
 	}
 	if os.Getenv("XDG_CACHE_HOME") != "" {
-		d.cache = filepath.Join(os.Getenv("XDG_CACHE_HOME"), module, appname)
+		d.dirs["cache"] = filepath.Join(os.Getenv("XDG_CACHE_HOME"), module, appname)
 	} else {
-		d.cache = filepath.Join(os.Getenv("HOME"), ".cache", module, appname)
+		d.dirs["cache"] = filepath.Join(os.Getenv("HOME"), ".cache", module, appname)
 	}
 
-	d.local = filepath.Join(os.Getenv("HOME"), ".local", module, appname)
-	d.logs = filepath.Join(d.local, "logs")
+	d.dirs["local"] = filepath.Join(os.Getenv("HOME"), ".local", module, appname)
+	d.dirs["logs"] = filepath.Join(d.dirs["local"], "logs")
 
-	makeUserDir(d.config)
-	makeUserDir(d.cache)
-	makeUserDir(d.local)
-	makeUserDir(d.logs)
+	d.makeUserDirs()
 }
 
-func (x *Dirs) Config() string {
-	return x.config
+func (d *DirData) Config() string {
+	return d.dirs["config"]
 }
 
-func (x *Dirs) Cache() string {
-	return x.cache
+func (d *DirData) Cache() string {
+	return d.dirs["cache"]
 }
 
-func (x *Dirs) Local() string {
-	return x.local
+func (d *DirData) Local() string {
+	return d.dirs["local"]
 }
 
-func (x *Dirs) Logs() string {
-	return x.logs
+func (d *DirData) Logs() string {
+	return d.dirs["logs"]
 }
 
-func makeUserDir(path string) {
-	err := os.MkdirAll(path, 0777)
-	if err != nil {
-		fmt.Printf("error creating userdir %s %v", path, err)
-		os.Exit(1)
+func (d *DirData) makeUserDirs() {
+	for dir := range d.dirs {
+		err := os.MkdirAll(dir, 0777)
+		if err != nil {
+			fmt.Printf("error creating userdir %s %v", dir, err)
+			os.Exit(1)
+		}
 	}
 }
